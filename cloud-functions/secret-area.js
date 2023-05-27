0f597b7ae27c26a5f75a76de937e8f002781c6cc
Marjolaine <mhebert@trisotech.com>
2023-05-27 08:46:15 -0400

    updated super secret area

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,4 @@
-exports.endeler = function (event, context, callback){
+exports.handeler = function (event, context, callback){
     callback(null, {
         statusCode: 200,
         body: "Welcome to the super secret area"
